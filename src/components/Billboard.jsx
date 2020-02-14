62f8e3d07502cb939c1edd9a293998cdb7ad1e8b
Student <example@example.com>
2020-02-13 16:38:37 -0800

    Import team photo to Billboard.

--- src/components/Billboard.jsx
+++ src/components/Billboard.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import Team from './team.jpg'
+
+var teamHeader= {
+  position: 'relative',
+  top: '-50px',
+  width: '100vw',
+  height: 'auto',
+  zIndex: '-1'
+
+}
 
 function Billboard(){
   return(
     <div>
-
+    <img style={teamHeader} src={Team}></img>
     </div>
   );
 }
